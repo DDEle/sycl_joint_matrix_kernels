b3dccd011136a113607bd5f3c29f5404425dabd0
Ding, Yi1 <yi1.ding@intel.com>
2024-12-03 09:24:36 +0000

    wip

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 run() {
     echo -e "\n\nRunning M$1 N$2 K$3"
     logfile="M$1_N$2_K$3.log"
-    icpx -fsycl -march=graniterapids -qopenmp -Xs '-device pvc' joint_matrix_bf16_fill_k_cache.cpp -DMATRIX_M=$1 -DMATRIX_N=$2 -DMATRIX_K=$3 -DPREFETCH &&
+    icpx -fsycl -Xs '-device pvc' joint_matrix_bf16_fill_k_cache.cpp -DMATRIX_M=$1 -DMATRIX_N=$2 -DMATRIX_K=$3 -DPREFETCH &&
         echo compiled! &&
         numactl -m0 -C0-51 time ./a.out 2>&1 | tee $logfile
     # icpx -fsycl joint_matrix_bf16_fill_k_cache.cpp -DMATRIX_M=$1 -DMATRIX_N=$2 -DMATRIX_K=$3 -DPREFETCH -DOOB && SYCL_PROGRAM_COMPILE_OPTIONS="-ze-opt-large-register-file" ./a.out 2>&1 | tee $logfile
